19d06bb934ce960f36ce345aec9749f2adce1d1e
Cal McArthur <cmcarthur@deeptrekker.com>
2022-04-13 11:15:42 -0400

    test script revisions

--- test_repeat_script.sh
+++ test_repeat_script.sh
@@ -7,11 +7,6 @@ FAIL_COUNTER=0
 END_BOOL=0
 START_BOOL=0
 START_COUNTER=0
-LINE_TO_PARSE=7
-REPO_NAME=""
-TEST_FILE=""
-
-rm output.txt 2> /dev/null || true
 
 while [ "$START_BOOL" -eq 0 ]
 do
@@ -22,10 +17,12 @@ do
     read -r TEST_FILE
     printf "Enter the number of times you want to run the test: "
     read -r NUMBER_TEST
+    printf "Enter 'q' for no output file. If output is desired, enter chosen file name (ex. output_file): "
+    read -r OUTPUT_FILE
 
     PATH_TO_FILE="${HOME}/${REPO_NAME}/build/bin/${TEST_FILE}"
 
-    if [ -f "$PATH_TO_FILE" ]
+    if [ -f "$PATH_TO_FILE" ];
         then
             START_BOOL=1
 
@@ -36,26 +33,41 @@ do
 
 done
 
+if [ "$OUTPUT_FILE" = "q" ];
+    then
+        OUTPUT_FILE=".${OUTPUT_FILE}.txt"
+        rm $OUTPUT_FILE 2> /dev/null || true
+    else
+        OUTPUT_FILE="${OUTPUT_FILE}.txt"
+        rm $OUTPUT_FILE 2> /dev/null || true
+fi
+
+LINE_TO_PARSE=7
+
 while [ $END_BOOL -eq 0 ];
 do
 
-    $PATH_TO_FILE >> "output.txt"
+    $PATH_TO_FILE >> $OUTPUT_FILE
 
     let MASTER_COUNTER++
     echo "Test ${MASTER_COUNTER} Running..."
 
     # ---------------------------------------------------
     # HARDCODED CONDITIONAL -- WILL NOT WORK FOR ALL TESTS
+    # TO TAILOR TO DIFFERENT TESTS:
+    # CHANGE INITIAL LINE (LINE_TO_PARSE=7), PASS KEY (OK), AND LINE INCREMENTS (+ 13, + 40)
     
-    LINE=$(sed "${LINE_TO_PARSE}q;d" output.txt)
+    LINE=$(sed "${LINE_TO_PARSE}q;d" ${OUTPUT_FILE})
 
     if [[ "$LINE" == *"OK"* ]];
 
         then 
+            # PASS
             let "PASS_COUNTER=PASS_COUNTER+1"
             let "LINE_TO_PARSE = LINE_TO_PARSE + 13"
         
         else
+            # FAIL
             let "FAIL_COUNTER=FAIL_COUNTER+1"
             let "LINE_TO_PARSE = LINE_TO_PARSE + 40"
     
@@ -74,5 +86,12 @@ do
     fi
 
 done
+
+if [ "$OUTPUT_FILE" = ".q.txt" ];
+    then
+        rm $OUTPUT_FILE 2> /dev/null || true
+    else
+        :
+fi
 
 echo "PASSES: ${PASS_COUNTER} | FAILS: ${FAIL_COUNTER}"
